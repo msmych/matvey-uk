76da0770c17251e4eb5e367288a111815703820f
Matvey <realsmych@gmail.com>
2024-03-04 21:40:33 +0000

    Env

--- app/src/main/kotlin/uk/matvey/app/StartServer.kt
+++ app/src/main/kotlin/uk/matvey/app/StartServer.kt
@@ -11,9 +11,16 @@ import io.ktor.server.netty.Netty
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import uk.matvey.app.AppConfig.Env
 
-fun startServer(wait: Boolean) {
-    embeddedServer(Netty, port = 8080, watchPaths = listOf("resources")) {
+fun startServer(config: AppConfig, wait: Boolean) {
+    System.setProperty("io.ktor.development", (config.env == Env.LOCAL).toString())
+    val watchPaths = if (config.env == Env.LOCAL) {
+        listOf("resources")
+    } else {
+        listOf()
+    }
+    embeddedServer(Netty, port = 8080, watchPaths = watchPaths) {
         install(FreeMarker) {
             templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
         }
