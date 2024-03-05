c00d590ce1c7e160347f5cf69c2c349a191c9032
Matvey <realsmych@gmail.com>
2024-03-05 22:20:07 +0000

    Keystore: save to file

--- app/src/main/kotlin/uk/matvey/app/StartServer.kt
+++ app/src/main/kotlin/uk/matvey/app/StartServer.kt
@@ -1,6 +1,8 @@
 package uk.matvey.app
 
 import freemarker.cache.ClassTemplateLoader
+import io.ktor.network.tls.certificates.buildKeyStore
+import io.ktor.network.tls.certificates.saveToFile
 import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.engine.applicationEngineEnvironment
@@ -16,8 +18,6 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import uk.matvey.app.AppConfig.Env
 import java.io.File
-import java.io.FileInputStream
-import java.security.KeyStore
 
 fun startServer(config: AppConfig, wait: Boolean) {
     val environment = applicationEngineEnvironment {
@@ -31,15 +31,20 @@ fun startServer(config: AppConfig, wait: Boolean) {
         }
         developmentMode = config.env == Env.LOCAL
         if (config.env == Env.PROD) {
-            val keyStoreFile = File("/certs/keystore.jks")
             val p12Pass = System.getenv("P12_PASS")
             val jksPass = System.getenv("JKS_PASS")
+            val keyStoreFile = File("/certs/keystore.jks")
+            val keyStore = buildKeyStore {
+                certificate("matveyAppCert") {
+                    password = p12Pass
+                    domains = listOf("matvey.uk", "127.0.0.1", "0.0.0.0", "localhost")
+                }
+            }
+            keyStore.saveToFile(keyStoreFile, jksPass)
             sslConnector(
-                keyStore = KeyStore.getInstance("JKS").apply {
-                    load(FileInputStream(keyStoreFile), jksPass.toCharArray())
-                },
+                keyStore = keyStore,
                 keyAlias = "matveyAppCert",
-                privateKeyPassword = { "".toCharArray() },
+                privateKeyPassword = { p12Pass.toCharArray() },
                 keyStorePassword = { jksPass.toCharArray() }) {
                 port = 8443
                 keyStorePath = keyStoreFile
